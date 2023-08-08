5d94852dce1d0121892a74cf8c7fc18996c9a3ce
dhaya1407 <dhayanithi1407@gmail.com>
2023-08-08 12:19:06 +0530

    new

---
+++ src/org/java/Practice.java
@@ -0,0 +1,16 @@
+package org.java;
+
+public class Practice {
+	
+	private void m1() {
+System.out.println("dhaya");
+	}
+	
+
+	public static void main(String[] args) {
+		Practice c = new Practice();
+		c.m1();
+		
+
+}
+}
