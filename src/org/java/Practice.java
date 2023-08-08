051e629801ecf8f002b73a1766c1945f91b52ad8
dhaya1407 <dhayanithi1407@gmail.com>
2023-08-08 12:25:14 +0530

    updated

--- src/org/java/Practice.java
+++ src/org/java/Practice.java
@@ -5,11 +5,15 @@ public class Practice {
 	private void m1() {
 System.out.println("dhaya");
 	}
+	private void m2() {
+System.out.println("rajesh");
+	}
 	
 
 	public static void main(String[] args) {
 		Practice c = new Practice();
 		c.m1();
+		c.m2();
 		
 
 }
